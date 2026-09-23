68e9cadcf800204e49a53d09ea9e2cea5b6ac4aa
Oscar Serrano <serranojaimesoscarsteven@hotmail.com>
2026-09-23 17:42:36 -0500

    chore(cleanup): remove unused components, scripts, svg images, and dependencies

--- crop-vans.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let data = fs.readFileSync('public/vans-logo.svg', 'utf8');
-data = data.replace('viewBox="0 0 192.756 192.756"', 'viewBox="10 75 170 35"');
-fs.writeFileSync('public/vans-logo.svg', data);
--- force-fill.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-['public/converse-logo.svg', 'public/vans-logo.svg', 'public/on-running-logo.svg'].forEach(file => {
-  let data = fs.readFileSync(file, 'utf8');
-  if (!data.includes('fill="white"')) {
-    data = data.replace('<svg', '<svg fill="white" ');
-    fs.writeFileSync(file, data);
-  }
-});
